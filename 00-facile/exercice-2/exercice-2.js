6babe2f02f653cc983c6c3b340f302000dc07e70
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-05-22 19:13:54 +0200

    Oops j'avais laissé la solution de l'exo 2 !

--- 00-facile/exercice-2/exercice-2.js
+++ 00-facile/exercice-2/exercice-2.js
@@ -1,8 +1,5 @@
 function exercice2(prenom) {
-  if (prenom !== undefined) {
-    return "Un pour " + prenom + ", un pour moi.";
-  }
-  return "Un pour toi, un pour moi.";
+  
 }
 
 // Ne touchez pas à la ligne suivante
